da7f70a4f7bbf53e34735652a68c1a8908eb0fa0
MSP <msp215721@gmail.com>
2024-06-25 09:28:39 +0530

    First step complete Now Start Setting with useState

--- learn/src/components/Product/Product.js
+++ learn/src/components/Product/Product.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Product.css';
+
+const currencyOptions = {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+    }
+
+    function getTotal(){
+        return total.toLocaleString(undefined,currencyOptions)
+    }
 export default function Product() {
+    const [cart, setCart] = useState([]);
+    const [total, setTotal] = useState([0]);
+
+
+
     return (
         <div className="wrapper">
             <div>
-                Shopping Cart: 0 total items.
+                Shopping Cart: {cart.length} total items.
             </div>
-            <div>Total: 0</div>
+            <div>Total: {getTotal()}</div>
             <div className="product"> <span role="img" aria-label="ice cream">
           🍦
         </span> </div>
